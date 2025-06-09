89080a5a004ae2cfcfdc0c65b885696e2c073b20
yuancye <yuanchaoye1986@gmail.com>
2025-06-09 10:28:12 -0700

    set map to dynamic client side

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,7 @@
 'use client';
+import dynamic from "next/dynamic";
 
-import Map from '@/components/Map';
+const Map = dynamic(() => import('@/components/Map'), { ssr: false });
 
 export default function Contact() {
   return (
